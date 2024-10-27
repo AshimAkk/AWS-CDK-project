d30e5ba8f086571bec17af46ce182baa43728f5b
Ashim <akkiyemba01@gmail.com>
2024-10-27 13:07:59 +1100

    created vpc and public subnet

--- cdk-vpc-project/lib/cdk-vpc-project-stack.ts
+++ cdk-vpc-project/lib/cdk-vpc-project-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class CdkVpcProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'CDK_VPC', {
+      maxAzs: 2,
+      subnetConfiguration: [{
+        name: 'Public',
+        subnetType: ec2.SubnetType.PUBLIC,
+        cidrMask: 24
+      }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkVpcProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
