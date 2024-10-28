7098902d5c8acae3635627e46c6dc4450cb52cb8
Ashim <akkiyemba01@gmail.com>
2024-10-28 18:41:52 +1100

    created the basic format and class for the EC2 stack

--- cdk-vpc-project/lib/ec2-stack.ts
+++ cdk-vpc-project/lib/ec2-stack.ts
@@ -1,3 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+/// Props
+
+interface EC2StackProps extends cdk.StackProps {
+
+}
+
+export class EC2Stack extends cdk.Stack {
+    constructor(scope:Construct, id: string, props: EC2StackProps) {
+        super(scope, id, props);
+    }
+}
