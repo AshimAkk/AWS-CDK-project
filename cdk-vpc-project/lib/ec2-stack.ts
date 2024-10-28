cc970ac297c1d30b92f917d43ef8e670f5d10572
Ashim <akkiyemba01@gmail.com>
2024-10-28 19:08:51 +1100

    created an EC2 instance in a private subnet

--- cdk-vpc-project/lib/ec2-stack.ts
+++ cdk-vpc-project/lib/ec2-stack.ts
@@ -5,11 +5,29 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2'
 /// Props
 
 interface EC2StackProps extends cdk.StackProps {
-
+    vpc: ec2.Vpc;
 }
 
 export class EC2Stack extends cdk.Stack {
-    constructor(scope:Construct, id: string, props: EC2StackProps) {
+    constructor(scope: Construct, id: string, props: EC2StackProps) {
         super(scope, id, props);
+
+
+        // EC2 instance 
+
+        const instance = new ec2.Instance(this, 'PrivateEC2', {
+            vpc: props.vpc,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+            },
+            machineImage: new ec2.AmazonLinuxImage({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)
+        })
+
+            cdk.Tags.of(instance).add('Name', 'PrivateEC2')
+
+
     }
 }
