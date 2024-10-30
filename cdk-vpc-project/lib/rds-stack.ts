3dbaecdbbd0f9a8407ff3324ca105f805b3c903f
Ashim <akkiyemba01@gmail.com>
2024-10-30 18:20:24 +1100

    set up interface to accept the VPC & created the basic stack structure

--- cdk-vpc-project/lib/rds-stack.ts
+++ cdk-vpc-project/lib/rds-stack.ts
@@ -3,3 +3,14 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';  
 import * as rds from 'aws-cdk-lib/aws-rds';
 
+// create an interface to accept the VPC from our VPC stack
+
+interface RDSStackProps extends cdk.StackProps {vpc: ec2.Vpc;}
+
+// Set up basic stack structure 
+
+export class RDSStack extends cdk.Stack { 
+    constructor(scope: Construct, id: string, props: RDSStackProps) {
+       super(scope, id, props) 
+    }
+}
