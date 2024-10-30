35a67b356d59776092e5deee51cc12dd74f097b5
Ashim <akkiyemba01@gmail.com>
2024-10-30 20:40:17 +1100

    inital rds setup & configuration + endpoint retrieval

--- cdk-vpc-project/lib/rds-stack.ts
+++ cdk-vpc-project/lib/rds-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';  
-import { Construct } from 'constructs';  
-import * as ec2 from 'aws-cdk-lib/aws-ec2';  
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 
 // create an interface to accept the VPC from our VPC stack
 
-interface RDSStackProps extends cdk.StackProps {vpc: ec2.Vpc;}
+interface RDSStackProps extends cdk.StackProps { vpc: ec2.Vpc; }
 
 // Set up basic stack structure 
 
-export class RDSStack extends cdk.Stack { 
+export class RDSStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: RDSStackProps) {
-       super(scope, id, props) 
+        super(scope, id, props)
+
+
+    // RDS Instance 
+
+        const rdsInstance = new rds.DatabaseInstance(this, 'RDS-instance', {
+            vpc:props.vpc,
+            databaseName: 'RDS-Instance',
+            engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0 }),
+            allocatedStorage: 20,
+            maxAllocatedStorage: 30,
+            multiAz: true,
+            deletionProtection: false,
+            backupRetention: cdk.Duration.days(0),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.LARGE),
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+            }
+        })
+        cdk.Tags.of(rdsInstance).add('Name', 'RDS-instance')
+
+        // print out the RDS Endpoint
+        const readEndPointProperty: rds.CfnDBCluster.ReadEndpointProperty = {
+            address: ''
+        }
+  
     }
+
 }
