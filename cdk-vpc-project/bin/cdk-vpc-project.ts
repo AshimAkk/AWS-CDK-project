c5535a6ad744b7e9d1f6ce719977e6996322d38c
Ashim <akkiyemba01@gmail.com>
2024-11-03 14:17:33 +1100

    updated App file and added the RDS instance stack

--- cdk-vpc-project/bin/cdk-vpc-project.ts
+++ cdk-vpc-project/bin/cdk-vpc-project.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkVpcProjectStack } from '../lib/cdk-vpc-project-stack';
 import { EC2Stack } from '../lib/ec2-stack';
+import  {RDSStack } from '../lib/rds-stack';
 
 const app = new cdk.App();
 const vpcStack = new CdkVpcProjectStack(app, 'CdkVpcProjectStack', {
@@ -13,4 +14,10 @@ new EC2Stack(app, 'Database-Instances', {
   vpc:vpcStack.vpc
 })
 
+new RDSStack(app, 'RDS-Instance', {
+  vpc:vpcStack.vpc
+})
+
 app.synth()
+
+
